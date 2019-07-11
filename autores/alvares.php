2087e1e68b19a81f2cdb095015c1f79b2523c9ba
Jennefer <jeny-barbosa@areacentral.github.com>
2019-07-11 17:13:49 -0300

    Arquivos Atualizados

---
+++ autores/alvares.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Alvares Azevedo</title>
+        <meta charset="utf-8">
+        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="estilo.css"/>
+
+    </head>
+    <body>
+        <?php require_once 'menu2.php'; ?>
+        <h1>Álvares de Azevedo</h1>
+
+
+        <table>
+            <th>
+            <td>
+                <img src="img/alvares.jpg" width="400" height="500" class="foto">
+            </td>
+            <td>
+                <p> Manuel Antônio Álvares de Azevedo, nasceu aos 12 de setembro de 1831, em São Paulo. Matriculou-se no curso de Direito em 1848 e deu início à produção literária, ao passo que começou a sentir os primeiros sintomas de tuberculose.
+                </p>
+
+                <p>Alguns dizem que o autor teve uma vida boêmia e para outros, uma vida casta. Contudo, suas poesias mostram uma idéia fixa em sua própria morte, provavelmente por saber do estado de saúde em que se encontrava. Entretanto, o poeta também passou pela experiência da morte prematura do irmão e de seus colegas de faculdade.</p>
+
+                <p> Inspirado pela literatura de Lord Byron e Musset, Álvares de Azevedo impregnou suas poesias com ares sarcásticos e irônicos e com idéias de autodestruição, morte, dor e de uma visão de amor irreal e idealizado por donzelas virgens. Além disso, seus poemas de temáticas de frustração e sofrimento ganham um ar melancólico por lembranças da infância, da mãe e da irmã.</p>
+
+                <p>Álvares de Azevedo parecia viver uma dualidade de sentimentos que são transpassadas para sua literatura: ora é meigo e sentimental, ora é mordaz e trágico. Por ter o pessimismo como âncora de seus poemas, foi considerado o responsável pelo “mal do século”, caracterizado pelo sentimento melancólico e pelo desencanto.
+                </p>
+
+                <p>Em 1851, a idéia de que a morte era certeira em sua vida, começou a escrever cartas à mãe, à irmã e aos amigos certificando-os do seu inevitável destino.
+                </p>
+
+                <p>Sua temática voltada à morte pode ser considerada também como um refúgio, uma fuga da realidade conturbada que vivia e da relação com o mundo que o cercava, o qual lhe dava sensação de impotência.</p>
+                <p>O poeta desejou tanto a morte que morreu ainda jovem, aos 20 anos, a 25 de abril de 1852.
+                </p>
+            </td>
+        </th>
+    </table>
+    <br>
+
+    <h1>Obras no nosso acervo</h1>
+    <fieldset>
+        <div class="gallery">
+            <a target="_blank" href="../livros/LiraDosVinteAnos.pdf">
+                <img src="../img/Lira20Anos.jpg" alt="Lira dos Vinte anos" width="800" height="500">
+                <div class="desc"><strong>Lira dos vinte anos</strong></div>
+            </a>
+        </div>
+
+    </fieldset>
+</body>
+</html>
